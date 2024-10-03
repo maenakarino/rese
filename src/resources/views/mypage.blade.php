59ac734e18e67261327cd7c23bf66abf7a4e319d
maenaka rino <rino.peach.7@gmail.com>
2024-10-03 15:02:27 +0900

    Mypageお気に入り店舗レイアウト修正

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -19,12 +19,12 @@
                         <div class="reservation__header">
                             <p class="header__title reservation__header__title">予約{{ $loop->iteration }}</p>
                             <div class="reservation__header-button">
-                                <form action="{{ route('reserve',$reserve) }}" method="get" class="header__form">
+                                <form action="" method="get" class="header__form">
                                     <button type="submit" class="form__button--edit" onclick="return confirmEdit()" title="予約変更">
                                         <img src="{{ asset('images/edit.svg') }}" alt="予約変更" class="form__button-img">
                                     </button>
                                 </form>
-                                <form action="{{ route('reserve',$reserve) }}" method="post"  class="header__form">
+                                <form action="" method="post"  class="header__form">
                                     @csrf
                                     @method('delete')
                                     <button type="submit" class="form__button--cancel" onclick="return confirmCancel()" title="予約キャンセル">
@@ -87,36 +87,70 @@
                 @endforeach
             </div>
 
-            
+            <label class="reservation__title hover__color--orange mobile-favorite__title">
+                <input type="radio" name="tab" class="reservation__title-input">お気に入り店舗
+            </label>
+            <div class="reservation__content-wrap mobile-favorite__wrap">
+                @foreach ($shops as $shop)
+            <div class="shop__content">
+                 <img class="shop__image" src="{{ $shop->image_url }}" alt="イメージ画像">
+        <div class="shop__item">
+            <span class="shop__title">{{ $shop->name }}</span>
+            <div class="shop__tag">
+                <p class="shop__tag-info">#{{ $shop->area->name }}</p>
+                <p class="shop__tag-info">#{{ $shop->genre->name }}</p>
+            </div>
+            <div class="shop__button">
+                <a href="/detail/{{ $shop->id }}?from=index" class="shop__button-detail">詳しくみる</a>
+                            @if (in_array($shop->id, $favorites->toArray()))
+                                <form action="{{ route('unfavorite', $shop) }}" method="post"
+                                    enctype="application/x-www-form-urlencoded" class="shop__button-favorite form">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="shop__button-favorite-btn" title="お気に入り削除">
+                                        <img class="favorite__btn-image" src="{{ asset('images/heart_red.png') }}">
+                                    </button>
+                                </form>
+                            @endif
+                               
+            </div>
+        </div>
+            </div>  
+                @endforeach
+            </div>
+        </div>
+    </div>
 
     <div class="favorite__wrap">
         <p class="favorite__title">お気に入り店舗</p>
-        <div class="shop__wrap">
-            @foreach ($shops as $shop)
-                <div class="shop__content">
-                    <img class="shop__image" src="{{ $shop->image_url }}" alt="イメージ画像">
-                    <div class="shop__item">
-                        <span class="shop__title">{{ $shop->name }}</span>
-                        <div class="shop__tag">
-                            <p class="shop__tag-info">#{{ $shop->area->name }}</p>
-                            <p class="shop__tag-info">#{{ $shop->genre->name }}</p>
-                        </div>
-                        <div class="shop__button">
-                            <a href="/detail/{{ $shop->id }}?from=mypage" class="shop__button-detail">詳しくみる</a>
-                            @if(in_array($shop->id, $favorites->toArray()))
-                                <form action="{{ route('unfavorite',$shop) }}" method="post" class="shop__button-favorite">
+    <div class="shop__wrap">
+    @foreach ($shops as $shop)
+    <div class="shop__content">
+        <img class="shop__image" src="{{ $shop->image_url }}" alt="イメージ画像">
+        <div class="shop__item">
+            <span class="shop__title">{{ $shop->name }}</span>
+            <div class="shop__tag">
+                <p class="shop__tag-info">#{{ $shop->area->name }}</p>
+                <p class="shop__tag-info">#{{ $shop->genre->name }}</p>
+            </div>
+            <div class="shop__button">
+                <a href="/detail/{{ $shop->id }}?from=index" class="shop__button-detail">詳しくみる</a>
+                            @if (in_array($shop->id, $favorites->toArray()))
+                                <form action="{{ route('unfavorite', $shop) }}" method="post"
+                                    enctype="application/x-www-form-urlencoded" class="shop__button-favorite form">
                                     @csrf
                                     @method('delete')
-                                        <button type="submit" class="shop__button-favorite-btn" title="お気に入り削除">
-                                            <img class="favorite__btn-image" src="{{ asset('images/heart_red.png') }}">
-                                        </button>
+                                    <button type="submit" class="shop__button-favorite-btn" title="お気に入り削除">
+                                        <img class="favorite__btn-image" src="{{ asset('images/heart_red.png') }}">
+                                    </button>
                                 </form>
                             @endif
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+                               
+            </div>
         </div>
+    </div>
+    @endforeach
+    </div>
     </div>
 </div>
 @endsection
