ff7a19f379d1c2bcd51e6570d2b08c47a2f6f264
maenakarino <rino.peach.7@gmail.com>
2024-11-22 16:03:58 +0900

    testユーザー追加

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,6 +16,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $param = [
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'password' => Hash::make(12345678)
+            ];
+        DB::table('users')->insert($param);
+
         User::factory()->count(10)->create();
     }
 }
